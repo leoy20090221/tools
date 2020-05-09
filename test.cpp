371955e14ef2d4156fb9247f3b85c6f37d7b3071
leoy20090221 <56961000+leoy20090221@users.noreply.github.com>
2020-05-09 19:24:21 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "tools.h"
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    int a[5];
+    cout << is_even(6) << endl;
+    ccin(a, 5);
+    for (int i = 0; i < 5; i++)
+        cout << a[i] << " ";
+    cout << endl;
+    cout << times2(9) << endl;
+    cout << is_ok(a, 5) << endl;
+    cout << min_array(a, 5) << endl;
+    return 0;
+}
