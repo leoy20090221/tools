7cfeefae9d608f5824bd30d84bd9cb52634c813f
leoy20090221 <56961000+leoy20090221@users.noreply.github.com>
2020-05-15 23:27:30 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -14,5 +14,14 @@ int main(int argc, const char *argv[]) {
     cout << big(a, 5) << endl;
     cout << small(a, 5) << endl;
     cout << min_array(a, 5) << endl;
+    cout << max_array(a, 5) << endl;
+    cout << temp_array(a, 5) << endl;
+    cout << minimize(1943L) << endl;
+    cout << maximize(1943L) << endl;
+    int a[9] = {3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int *wei = closeimize(a, 9);
+    for (int i = 0; i < 9; i++)
+        cout << wei[i] << " ";
+    cout << endl;
     return 0;
 }
